0bf23c2f8ee746d961201fd75ebcf7dae78c27c3
Miguel Viana <lmiguelviana@hotmail.com>
2026-05-13 20:46:41 -0300

    fix: switch to direct Supabase connection (bypass pgBouncer pooler issue)

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -5,7 +5,6 @@ import * as schema from './schema'
 const client = postgres(process.env.DATABASE_URL!, {
   ssl: 'require',
   max: 1,
-  prepare: false,
 })
 
 export const db = drizzle(client, { schema })
